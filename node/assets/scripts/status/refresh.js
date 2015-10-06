152ab8d60c67e30f237e655b821073203b679b45
Jeff Horton <jeff@goblinoid.co.uk>
2015-10-07 00:23:43 +0100

    tabs -> spaces

--- node/assets/scripts/status/refresh.js
+++ node/assets/scripts/status/refresh.js
@@ -1,43 +1,43 @@
 $(function(){
-	var url = '/status.json'; //todo reverse routing in JS
-	var interval = 5000; // 5 seconds
+    var url = '/status.json'; //todo reverse routing in JS
+    var interval = 5000; // 5 seconds
 
-	function getAjaxResponse(request) {
+    function getAjaxResponse(request) {
         return Bacon.fromPromise($.ajax(request))
     }
 
-	var request = {url: url}
+    var request = {url: url}
 
-	var updateStream =
-	    Bacon.once(request)
-			.merge(Bacon.interval(interval, request))
-			.flatMap(getAjaxResponse);
+    var updateStream =
+        Bacon.once(request)
+            .merge(Bacon.interval(interval, request))
+            .flatMap(getAjaxResponse);
 
-	$('.world-terror-dial .hand').each(function(){
-		var $hand = $(this);
+    $('.world-terror-dial .hand').each(function(){
+        var $hand = $(this);
 
-		updateStream.onValue(function(gameStatus)
-		{
-			var rotation = gameStatus.terrorLevel === undefined ? -90 : gameStatus.terrorLevel;
+        updateStream.onValue(function(gameStatus)
+        {
+            var rotation = gameStatus.terrorLevel === undefined ? -90 : gameStatus.terrorLevel;
 
-			$hand.css({
-            	transform: 'rotate(' + rotation + 'deg)'
+            $hand.css({
+                transform: 'rotate(' + rotation + 'deg)'
             });
-		})
-	});
+        })
+    });
 
-	$('.pr-list').each(function(){
-		var $list = $(this)
-		var countryName = $list.data('country');
+    $('.pr-list').each(function(){
+        var $list = $(this)
+        var countryName = $list.data('country');
 
-		updateStream.onValue(function(gameStatus)
-	    {
-	        if(gameStatus.countryPRs !== undefined && gameStatus.countryPRs[countryName] !== undefined)
-	        {
-	            var newPr = gameStatus.countryPRs[countryName];
-	            $list.find('.pr-item').removeClass('current');
-	            $list.find('.pr-item[data-pr-level=' + newPr + ']').addClass('current');
-	        }
-	    });
-	});
+        updateStream.onValue(function(gameStatus)
+        {
+            if(gameStatus.countryPRs !== undefined && gameStatus.countryPRs[countryName] !== undefined)
+            {
+                var newPr = gameStatus.countryPRs[countryName];
+                $list.find('.pr-item').removeClass('current');
+                $list.find('.pr-item[data-pr-level=' + newPr + ']').addClass('current');
+            }
+        });
+    });
 });
