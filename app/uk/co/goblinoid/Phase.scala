3ba13cd9d6af71222823734880cf03d23af2e286
Jeff Horton <jeff@goblinoid.co.uk>
2015-11-06 23:40:11 +0000

    Update Phase Definitions

--- app/uk/co/goblinoid/Phase.scala
+++ app/uk/co/goblinoid/Phase.scala
@@ -17,7 +17,7 @@ object Phase {
       "Operations Phase (Allocation)", 
       Seq(
         Activity("UN Ambassadors", "Go to the Security Council table."), 
-        Activity("Heads of State","Buy influence cards from nation control."), 
+        Activity("Diplomats","Buy influence cards from nation control."), 
         Activity("Generals", "Goto world map with interceptors, special agents, and megabucks needed for the turn."), 
         Activity("Scientists", "Buy research points from science control.")
       ),
@@ -27,7 +27,7 @@ object Phase {
       "Operations Phase (Place Units)",
       Seq(
         Activity("UN Ambassadors", "Discuss current issues and crises. Draft and agree on any security council resolutions."),
-        Activity("Heads of State/Other Diplomats", "Circulate and conduct diplomacy. May visit the UN, but not interrupt."),
+        Activity("Diplomats", "Circulate and conduct diplomacy. May visit the UN, but not interrupt."),
         Activity("Generals", "Place units and resources in the ready box. Then aliens place ships. Then generals may place or move units."),
         Activity("Scientists", "Conduct research with science control. Arrange a science conference or declare a trade fair. May liaise with other players, but not go to the UN or World Map.")
       ),
@@ -37,7 +37,7 @@ object Phase {
       "Operations Phase (Resolve Interceptions)",
       Seq(
         Activity("UN Ambassadors", "Discuss current issues and crises. Draft and agree on any security council resolutions."),
-        Activity("Heads of State/Other Diplomats", "Circulate and conduct diplomacy. May visit the UN, but not interrupt."),
+        Activity("Diplomats", "Circulate and conduct diplomacy. May visit the UN, but not interrupt."),
         Activity("Generals", "Control will resolve interceptions. Human and alien command players should be ready for any resolutions they are involved in."),
         Activity("Scientists", "Conduct research with science control. Arrange a science conference or trade fair. May liaise with other players, but not go to the UN or World Map.")
       ),
@@ -47,7 +47,7 @@ object Phase {
       "Operations Phase (Resolve Ground Forces)",
       Seq(
         Activity("UN Ambassadors", "Discuss current issues and crises. Draft and agree on any security council resolutions."),
-        Activity("Heads of State/Other Diplomats", "Circulate and conduct diplomacy. May visit the UN, but not interrupt."),
+        Activity("Diplomats", "Circulate and conduct diplomacy. May visit the UN, but not interrupt."),
         Activity("Generals", "Aliens declare missions they wish to launch. Resolve ground conflicts."),
         Activity("Scientists", "Conduct research with science control. Arrange a science conference or trade fair. May liaise with other players, but not go to the UN or World Map.")
       ),
@@ -74,7 +74,8 @@ object Phase {
       Seq(
         Activity("Diplomats", "May go to the world map to resolve political/agent actions."),
         Activity("Scientists", "May attend ONE science conference OR trade fair."),
-        Activity("Other players", "May circulate to discuss plans, make agreements, threaten, cajole and flatter.")
+        Activity("UN Ambassadors", "May reconvene Security Council if necessary. May take mandate to world map."),
+        Activity("Other players", "May circulate to discuss plans, make agreements, threaten, cajole, and flatter.")
       ),
       Duration.ofMinutes(14)
     ),
